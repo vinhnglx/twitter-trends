1b45e3d7d3c2ef34b8802ed35eb8e6e589e49bf4
Vinh Nguyen <vinh.nglx@gmail.com>
2015-10-02 09:30:04 +0700

    Rename environment variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"],
+  provider :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"],
   {
     :secure_image_url => 'true',
     :image_size => 'original'
