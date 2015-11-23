f58f5fde5582ed2a8b4a02ebcea0023fbb35d16e
Vinh Nguyen <vinh.nglx@gmail.com>
2015-11-23 23:12:56 +0700

    Config for rubocop, rails_best_practice, brakeman, scss_lint

--- scripts/saddler.sh
+++ scripts/saddler.sh
@@ -6,6 +6,10 @@ gem install --no-document rubocop rubocop-checkstyle_formatter \
     checkstyle_filter-git saddler saddler-reporter-github \
     scss_lint scss_lint_reporter_checkstyle
 
+echo "********************"
+echo "* RuboCop          *"
+echo "********************"
+
 git diff -z --name-only origin/master \
  | xargs -0 rubocop-select \
  | xargs rubocop \
@@ -13,5 +17,37 @@ git diff -z --name-only origin/master \
      --format RuboCop::Formatter::CheckstyleFormatter \
  | checkstyle_filter-git diff origin/master \
  | saddler report \
+    --require saddler/reporter/github \
+    --reporter Saddler::Reporter::Github::PullRequestReviewComment
+
+echo "***********************"
+echo "* Rails Best Pratices *"
+echo "***********************"
+rails_best_practices -f xml
+
+cat rails_best_practices_output.xml \
+  | checkstyle_filter-git diff origin/master \
+  | saddler report \
+    --require saddler/reporter/github \
+    --reporter Saddler::Reporter::Github::PullRequestReviewComment
+
+echo "********************"
+echo "* Brakeman         *"
+echo "********************"
+brakeman -o brakeman.json
+cat brakeman.json \
+  | brakeman_translate_checkstyle_format translate \
+  | checkstyle_filter-git diff origin/master \
+  | saddler report \
+    --require saddler/reporter/github \
+    --reporter Saddler::Reporter::Github::PullRequestReviewComment
+
+echo "********************"
+echo "* SCSS-Lint        *"
+echo "********************"
+scss-lint --require=scss_lint_reporter_checkstyle --format=Checkstyle -o scsslint.xml
+cat scsslint.xml \
+  | checkstyle_filter-git diff origin/master \
+  | saddler report \
     --require saddler/reporter/github \
     --reporter Saddler::Reporter::Github::PullRequestReviewComment
